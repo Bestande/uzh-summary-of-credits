fb49bcd16caf2999f30579bc1a347bb9698bbd26
Jonny Burger <jonathanburger11@gmail.com>
2019-10-06 01:46:46 +0200

    add demo

---
+++ example.js
@@ -0,0 +1,12 @@
+var lib = require('./');
+
+lib
+	.all('<username>', '<password>', require('node-fetch'), function(progress) {
+		console.log(progress);
+	})
+	.then(function(result) {
+		console.log('done', result);
+	})
+	.catch(function(err) {
+		console.log(err);
+	});
